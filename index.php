ae70363e129638c9497c4feb261d7b2e9e4a7be2
jasminekumari <150835881+jasminekumari@users.noreply.github.com>
2023-11-14 22:31:10 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<head>
+    <?php include "headerfiles.html" ?>
+    <title>Matrimony.</title>
+</head>
+
+<body>
+<?php include "publicheader.html" ?>
+<!-- Main Wraapper -->
+<div class="main_wrapper">
+    <!-- Banner Wraapper -->
+    <div class="single_section" data-scroll-index='1'>
+        <section class="wed_banner_wrapper relative">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="wed_banner_text fadeInLeft wow" data-wow-delay="200ms" data-wow-duration="1500ms"
+                             style="visibility: visible; animation-duration: 1500ms; animation-delay: 200ms; animation-name: fadeInLeft;">
+                            <img src="assets/images/title-bg.png" alt="">
+                            <h6></h6>
+                            <h1>Soulmate</h1>
+                            <p>We bring hearts together."</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="wed_bottom_shape"></div>
+            </div>
+        </section>
+    </div>
+    <!-- Ring section -->
+    <div class="wed_ring_section">
+        <div class="container">
+            <div class="row">
+                <div class="wed_guest_infobox slideInUp wow" data-wow-delay="200ms" data-wow-duration="1500ms"
+                     style="visibility: visible; animation-duration: 1500ms; animation-delay: 200ms; animation-name: slideInUp;">
+
+                    <div class="card bg-glass background-radiant "
+                         style="background-color: #FA8072  ; backdrop-filter:blur(3px)">
+                        <div class="card-body px-4 py-5 px-md-5">
+                            <h2 class="text-center " style=" color: floralwhite">Why <span>Soulmate</span> Matrimonial?
+                            </h2>
+
+                            <div class="col-md-8 col-12 mx-auto pt-3  box-min-width-text1" style=" color: floralwhite">
+                                We are the Solmate matchmaking service, and offer the most exclusive database of
+                                truly soulmate matches for you!
+                            </div>
+                            <div class="col-md-8 col-12 mx-auto pt-3 md-text1 box-min-width-text2"
+                                 style=" color: floralwhite">
+                                At Solmate Matrimony, we help you find the perfect life partner who matches your
+                                stature, aura, and sophistication in every sense of the word.
+                            </div>
+                            <p class="text-center" style="color: whitesmoke"></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Guest section -->
+    <div class="wed_guest_section">
+        <div class="wed_guest_overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="wed_guest_heading">
+
+                        <img src="assets/images/heading_white.png" alt="">
+                    </div>
+                </div>
+
+                <div class="wed_guest_infobox slideInUp wow" data-wow-delay="200ms" data-wow-duration="1500ms"
+                     style="visibility: visible; animation-duration: 1500ms; animation-delay: 200ms; animation-name: slideInUp;">
+                    <h2 class="text-center " style=" color: floralwhite">How <span>it</span> Works?</h2>
+
+                    <div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="images/wd2.jpeg" class="card-img-top"
+                                     alt="Skyscrapers"/>
+                                <div class="card-body " style="background-color:#FA8072 ">
+                                    <h5 class="card-title">We provide you detailed profile</h5>
+
+                                    <p class="card-text mt-2 ">We provide you all the details required to find the
+                                        perfect match and your true soulmate.
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="images/wd1.jpeg" class="card-img-top"
+                                     alt="Los Angeles Skyscrapers"/>
+                                <div class="card-body" style="background-color:#FA8072 ">
+                                    <h5 class="card-title">We help you discover your perfect match</h5>
+                                    <p class="card-text">We recommend matches as per your expectations, helping you
+                                        connect with prospects and assisting you to find your perfect match.</p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="images/wd3.jpeg" class="card-img-top"
+                                     alt="Palm Springs Road"/>
+                                <div class="card-body" style="background-color:#FA8072 ">
+                                    <h5 class="card-title">We help you to connect with them easily</h5>
+                                    <p class="card-text">
+                                        We help you to connect with your matches through calls and messages.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+</div>
+<!-- service section  -->
+<div class="wed_service_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="wed_service_heading">
+                    <h2>Successful Stories</h2>
+                    <img src="assets/images/heading.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="wed_service_slider slideInUp wow" data-wow-delay="200ms" data-wow-duration="1500ms"
+                     style="visibility: visible; animation-duration: 1500ms; animation-delay: 200ms; animation-name: slideInUp;">
+                    <div class="swiper-container swiper_service_slider">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="wed_ser_box">
+                                    <img src="images/s1.jpeg"  style="height:340px; width: 340px">
+                                    <div class="service-info">
+                                        <h4 style="color: #FA8072">Jasika and Sidharth</h4>
+                                        <p>Mrs Sanjita was looking for a well-educated bride with good
+                                            family background, preferably settled in India for his son; Sidharth. He was
+                                            looking for a beautiful girl with perfect stars and horoscope.<span
+                                                    class="xs-none"> As soon as we understood the family's preferences, we started shortlisting profiles with matching horoscopes and sent them to the family. Lighting struck in almost no time! Jasika Ghoshal's profile met all the expectations shared and got into marriage super soon.
+                                    </p>
+
+                                    </div>
+                                </div>
+                                </div>
+
+                        <div class="swiper-slide">
+                            <div class="wed_ser_box">
+                                <img src="images/s3.jpg"  style="height:340px; width: 340px">
+                                <div class="service-info">
+                                    <h4 style="color: #FA8072">Kartik and Soumaya</h4>
+                                    <p class="xs-small-text">Kartik joined Solmate matrimony and was shown several good profiles. Soumaya's profile matched his and his family's expectations. Soumaya and Kartik started taking once mutually agreed and and very quickly things fell into place and they have now tied knot and are happy.
+                                </p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="wed_ser_box">
+                                <img src="images/s2.jpg"  style="height:340px; width: 340px">
+                                <div class="service-info">
+                                    <h4 style="color: #FA8072">Anshika and Tushar</a></h4>
+                                    <p>Tushar was searching for perfect match for himself .So, He found Anshika's  profile matched perfectly with his expectations! They spoke and their families began a telephonic<span class="xs-none"> correspondence, what followed was a lovely meet up of both families. Both the families were impressed with each other. Their views, traditional values, and other core principles fell into place perfectly. Post some more meetings the wedding was finalised.
+                                </div>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        </div>
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Event Section -->
+
+<!-- Gallery Section Start-->
+<div class="single_section" data-scroll-index='4'>
+    <section class="wed_gallery_wrapper">
+        <!-- <div class="wed_top_shape"></div> -->
+        <div class="container">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12 text-center">
+                <div class="wed_service_heading wed_gal_heading">
+                    <h2> Photo Gallery</h2>
+                    <img src="assets/images/heading.png" alt="bottom-img">
+                </div>
+            </div>
+            <div class="wed_gallery_slides popup_gallery slideInLeft wow" data-wow-delay="200ms"
+                 data-wow-duration="1500ms"
+                 style="visibility: visible; animation-duration: 1500ms; animation-delay: 200ms; animation-name: slideInLeft;">
+                <div class="wedingService swiper-container">
+                    <div class="swiper-wrapper">
+                        <!-- Slide One -->
+                        <div class="swiper-slide">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="wed_gallery_section mb_30">
+                                        <a href="images/g3.jpeg" title=""><img
+                                                    src="images/g3.jpeg" alt=""/>
+                                            <div class="wed_galsec_overlay">
+                                                <p>Making a love</p>
+                                                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="wed_gallery_section mb_30">
+                                        <a href="assets/images/gallery/gall-imgL2.png" title=""><img
+                                                    src="assets/images/gallery/gall-img2.png" alt=""/>
+                                            <div class="wed_galsec_overlay">
+                                                <p>Making a love</p>
+                                                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="wed_gallery_section mb_30">
+                                        <a href="images/g2.jpg" title=""><img
+                                                    src="images/g2.jpg" alt=""/>
+                                            <div class="wed_galsec_overlay">
+                                                <p>Making a love</p>
+                                                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Slide Two -->
+                        <div class="swiper-slide">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="wed_gallery_section mb_30">
+                                        <a href="assets/images/gallery/gall-imgL4.png" title=""><img
+                                                    src="assets/images/gallery/gall-img4.png" alt=""/>
+                                            <div class="wed_galsec_overlay">
+                                                <p>Making a love</p>
+                                                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="wed_gallery_section mb_30">
+                                        <a href="assets/images/gallery/gall-imgL5.png" title=""><img
+                                                    src="assets/images/gallery/gall-img5.png" alt=""/>
+                                            <div class="wed_galsec_overlay">
+                                                <p>Making a love</p>
+                                                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="wed_gallery_section mb_30">
+                                        <a href="images/g1.jpg" title=""><img
+                                                    src="images/g1.jpg" alt=""/>
+                                            <div class="wed_galsec_overlay">
+                                                <p>Making a love</p>
+                                                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Slider Dots -->
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<!-- Testimonial Section -->
+
+<!-- When and Where Section Start -->
+
+<!-- Contact Section -->
+
+<!-- gift registry -->
+</div>
+
+
+<?php include "footer.html";
+include "footer_scripts.html"; ?>
+</body>
+</html>
